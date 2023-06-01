8147286ca84afe6a08401d72de14a9ea0108ea6d
99winnmin <ojysep9987@gmail.com>
2023-06-02 01:33:08 +0900

    [Feat] DE:BOOST 분석 API 추가

--- app.py
+++ app.py
@@ -1,11 +1,23 @@
-from flask import Flask
+from flask import Flask, request
+import boto3, json
+import deboost_runner
 
 app = Flask(__name__)
+s3 = boto3.client('s3')
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+@app.route('/analysis', methods=['POST'])
+def data_analysis():
+    if request.method == 'POST':
+        data = request.json
+        bucket_name = data['bucketName']
+        object_key = data['objectKey']
+        response = s3.get_object(Bucket=bucket_name, Key=object_key)
+        file_content = response['Body'].read().decode('utf-8')
+        data = json.loads(file_content)
+        result = deboost_runner.run(data)
+        print(result)
+        return result
 
 
 if __name__ == '__main__':
