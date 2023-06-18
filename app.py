0ac50a7290385eb8b1c99e7ab8ff601ec83a3328
99winnmin <ojysep9987@gmail.com>
2023-06-19 02:14:27 +0900

    [Fix] Flask 리턴값 변경

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, request
+from flask import Flask, request, jsonify
 import boto3, json
 import deboost_runner
+import deboost
+import numpy as np
 
 app = Flask(__name__)
 s3 = boto3.client('s3')
@@ -10,15 +12,24 @@ s3 = boto3.client('s3')
 def data_analysis():
     if request.method == 'POST':
         data = request.json
+        user_name = data['summonerName']
         bucket_name = data['bucketName']
-        object_key = data['objectKey']
-        response = s3.get_object(Bucket=bucket_name, Key=object_key)
-        file_content = response['Body'].read().decode('utf-8')
-        data = json.loads(file_content)
-        result = deboost_runner.run(data)
+        object_key_list = data['keyNames']
+        input_data_list = []
+        for key in object_key_list:
+            response = s3.get_object(Bucket=bucket_name, Key=key)
+            file_content = response['Body'].read().decode('utf-8')
+            data = json.loads(file_content)
+            input_data_list.append(data)
+
+        result = deboost.main(input_data_list, user_name)
+        # result = deboost_runner.run(data)
+        result = np.round(result*100, 2).astype(float).tolist()
+        result = [round(x, 3) for x in result]
         print(result)
-        return result
+        return jsonify(data=result)
 
 
 if __name__ == '__main__':
-    app.run()
+    # app.run()
+    app.run(host='0.0.0.0', port=5000)
